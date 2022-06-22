b56b56f3c26039f44af16f212999f45ab5f1cc7b
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-06-22 10:40:56 -0300

    create a hash of a user's password and authenticate him

--- hash/hash.js
+++ hash/hash.js
@@ -1,1 +1,34 @@
 import { createHash } from 'crypto';
+
+function criaHash(senha){
+    return createHash('sha256').update(senha).digest('hex');
+}
+
+console.log(criaHash("uma String Qualquer"));
+console.log(criaHash("uma string qualquer"));
+
+class Usuario{
+    constructor(nome, senha){
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+
+    autentica(nome, senha){
+        if(nome === this.nome && this.hash === criaHash(senha)){
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        }
+        console.log("Usuário ou senha incorretos.");
+        return false;
+    }
+}
+
+const usuario = new Usuario('Vitor', 'minhaSenha');
+console.log(usuario);
+
+//Caso de sucesso
+usuario.autentica('Vitor', 'minhaSenha');
+
+//Casos de fracasso
+usuario.autentica('Vit', 'minhaSenha');
+usuario.autentica('Vitor', 'minhasenha');
